e6c7f18834208807a72b9b911ffe3af6160d2bc6
David Margery <david.margery@inria.fr>
2015-09-01 17:15:57 +0200

    Added an example __init.py file to ease installation

---
+++ gcf_testbedname_plugin/__init__.py
@@ -0,0 +1,14 @@
+"""
+gcf_bonfire_plugin
+==================
+
+<insert nifty doc string here>
+"""
+
+__title__ = 'gcf_bonfire_plugin'
+__version__ = '0.1'
+__author__ = 'David Margery <david.margery@inria.fr>'
+__license__ = 'CeCILL-B'
+__copyright__ = 'Copyright 2015 Inria - contributor(s): David Margery'
+
+from .testbed import MyTestbedDelegate
