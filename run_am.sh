f5310d301d7c0732d0e3e29c5981090bad834ac6
Arthur Garnier <arthur.garnier@inria.fr>
2016-07-14 15:35:40 +0200

    Fix pythonpath

--- run_am.sh
+++ run_am.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 
-PYTHONPATH="${PYTHONPATH}:$(pwd)/gcf_testbedname_plugin"
-echo $PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:$(pwd)/gcf_testbedname_plugin/"
 python2 geni-tools/src/gcf-am.py -V 3 -c gcf_testbedname_plugin/gcf_config
