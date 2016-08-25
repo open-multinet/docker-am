2e40ebe7400de6ed06bd6ab593920bc88ac28262
Arthur Garnier <arthur.garnier@inria.fr>
2016-08-25 16:57:27 +0200

    Add comments

--- gcf_docker_plugin/extendedresource.py
+++ gcf_docker_plugin/extendedresource.py
@@ -12,7 +12,7 @@ class ExtendedResource(Resource):
         self.error = ""
         self.host = "127.0.0.1"
 
-    #Return a etree.Element containing advertisement info
+    #Returns a etree.Element containing advertisement info
     #You should have to add some information like sliver_type
     def genAdvertNode(self, _urn_authority, _my_urn):
         r = etree.Element("node")
@@ -25,28 +25,38 @@ class ExtendedResource(Resource):
         r.set("exclusive", "false")
         return r
 
+    #Returns the resource if matches the component_id (if given)
+    #In case of a pool, you should search for the resource which match
     def getResource(self, component_id=None):
         if component_id is not None:
             if component_id != self.id:
                 return None
         return self
 
+    #This method is called when the Allocate call fails after the allocation of the resources (for example problem with allocation time)
     def deallocate(self):
         self.available = True
         pass
 
+    #Returns the ssh port to reach the resource
     def getPort(self):
         return self.ssh_port
 
+    #Returns list of configured users
     def getUsers(self):
         return self.users.keys()
 
+    #Provisionning take some time but the Provision call done to the API must reply quickly.
+    #So, preprovision only prepare required value like users to be configured, SSH port used, ... 
     def preprovision(self, user, ssh_keys):
         pass
 
+    #Do all the stuff to set up the resource, like load the proper OS, configure users and SSH authorized keys, ...
+    #Should return True, or set self.error with an error message and return False
     def provision(self, user, keys):
         pass
 
+    #Returns an etree Element node with authentification information
     def manifestAuth(self):
         if len(self.getUsers())==0:
             return []
@@ -61,11 +71,15 @@ class ExtendedResource(Resource):
                 ret.append(auth)
             return ret
 
+    #A "blocking" method which return True when the SSH connection is available = The node is fully ready for the user
+    #Should return True, or set self.error with an error message and return False
     def checkSshConnection(self):
         pass
 
+    #Decompress the target of the url to install_path on the resource
     def installCommand(self, url, install_path):
         pass
 
+    #Executes the command given with the shell provided of the resource
     def executeCommand(self, shell, cmd):
         pass
