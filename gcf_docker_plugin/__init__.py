18c974c3aef0bfc260067a59b51e8f1d651e67a5
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2017-07-11 15:44:43 +0200

    fixed __init__.py

--- gcf_docker_plugin/__init__.py
+++ gcf_docker_plugin/__init__.py
@@ -1,14 +1,17 @@
 """
-gcf_bonfire_plugin
+gcf_docker_plugin
 ==================
 
-<insert nifty doc string here>
 """
 
-__title__ = 'gcf_bonfire_plugin'
-__version__ = '0.1'
-__author__ = 'David Margery <david.margery@inria.fr>'
-__license__ = 'CeCILL-B'
-__copyright__ = 'Copyright 2015 Inria - contributor(s): David Margery'
+__title__ = 'gcf_docker_plugin'
+__version__ = '1.0'
+__author__ = 'David Margery <david.margery@inria.fr>, Arthur Garnier <arthur.garnier@inria.fr>, Wim Van de Meerssche <wim.vandemeerssche@ugent.be>'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2015 - contributor(s): David Margery, Arthur Garnier, Wim Van de Meerssche'
 
-from .testbed import MyTestbedDelegate
+from .testbed import DockerAggregateManager
+from dockercontainer import DockerContainer
+from dockermaster import DockerMaster
+from gcf_to_docker import DockerManager
+from extendedresource import ExtendedResource
